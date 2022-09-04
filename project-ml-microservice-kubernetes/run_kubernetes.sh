8069b224a360e887c3cb6d38d1e59f43485f337a
Ubuntu <ubuntu@ip-172-31-71-168.ec2.internal>
2022-09-04 21:17:52 +0000

    final push

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="khalilone62/flaskapp:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskapp --image=khalilone62/flaskapp:v1.0.0 --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+sleep 100
+kubectl port-forward flaskapp 8000:80
